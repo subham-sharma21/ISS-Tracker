6c0c78bedf0e64a786acb5e42aa206d4a125d18c
electro <bitone9437@gmail.com>
2024-12-29 22:20:27 +0530

    text_highlight

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// ISS Latitude + Longitude data
 async function fetchISSLocation() {
-    try {
-      const response = await fetch(
-        "https://api.wheretheiss.at/v1/satellites/25544"
-      );
+  try {
+      const response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
       const data = await response.json();
-  
+
       const latitudeElement = document.getElementById("latitude");
       const longitudeElement = document.getElementById("longitude");
       const speedElement = document.getElementById("speed");
-  
-      latitudeElement.textContent = data.latitude;
-      longitudeElement.textContent = data.longitude;
-      speedElement.textContent = data.velocity;
-      document.getElementById("numberOfPeople").textContent = data.footprint;
-  
+
+      // Formatting latitude, longitude, and speed to 2 decimal places
+      latitudeElement.textContent = data.latitude.toFixed(2);
+      longitudeElement.textContent = data.longitude.toFixed(2);
+      speedElement.textContent = data.velocity.toFixed(2);
+      document.getElementById("numberOfPeople").textContent = data.footprint.toFixed(0);
+
       setTimeout(fetchISSLocation, 1000); // Fetch data every 5 seconds
-    } catch (error) {
+  } catch (error) {
       console.error("Error fetching ISS data:", error);
-    }
   }
-  
-  fetchISSLocation(); // Start fetching ISS data
+}
+
+fetchISSLocation(); // Start fetching ISS data
